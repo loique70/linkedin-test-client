41373d65df33766a75ee24ec9be1ced554a555ef
loique70 <lfogouang39@gmaol.com>
2023-12-21 02:27:51 -0800

    correction of url

--- src/api/sectors.ts
+++ src/api/sectors.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_URL = "https://linkedin-test-api.vercel.app/";
+
 export const fetchSectors = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/sectors");
+    const response = await axios.get(`${API_URL}/sectors`);
     return response.data;
   } catch (error) {
     console.error("Error fetching sectors", error);
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,9 +1,11 @@
 // api/users.ts
 import axios from "axios";
 
+const API_URL = "https://linkedin-test-api.vercel.app/";
+
 export const fetchSectors = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/sectors");
+    const response = await axios.get(`${API_URL}/sectors`);
     return response.data;
   } catch (error) {
     console.error("Error fetching sectors", error);
@@ -12,7 +14,7 @@ export const fetchSectors = async () => {
 
 export const createUser = async (userData: any) => {
   try {
-    const response = await axios.post("http://localhost:8080/users", userData);
+    const response = await axios.post(`${API_URL}/users`, userData);
     return response.data;
   } catch (error) {
     console.error("Error saving data", error);
@@ -21,10 +23,7 @@ export const createUser = async (userData: any) => {
 
 export const updateUser = async (userId: any, userData: any) => {
   try {
-    const response = await axios.put(
-      `http://localhost:8080/users/${userId}`,
-      userData
-    );
+    const response = await axios.put(`${API_URL}/users/${userId}`, userData);
     return response.data;
   } catch (error) {
     console.error("Error updating data", error);
